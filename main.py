2c753118b27cb3aeed2433fc78251fed72771db9
Luke C <lpcolias@gmail.com>
2023-10-20 17:59:17 -0700

    Start work on DnD handling on chess board

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from calendar import c
 import sys
 from GUI.ChessGui import ChessGUI
 import tkinter as tk
-
+import chess
 __small_board = 64
 __large_board = 96
 
@@ -16,15 +17,17 @@ def main():
         
         if arg == '-play':
             break
-        elif arg == '-sim':
             break 
    
-    root = tk.Tk()
+    board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
 
-    root.wm_attributes('-transparentcolor', 'grey')
-
-    chessGUI = ChessGUI(root, __small_board)
-    root.mainloop()
+    chess.svg.piece(chess.Piece.from_symbol("R"))
+    
+    # Create tkinter root object 
+    #root = tk.Tk()
+    # root.wm_attributes('-transparentcolor', 'grey') 
+    #chessGUI = ChessGUI(root, __small_board)
+    #root.mainloop()
 
 if __name__ == '__main__':
     main()
